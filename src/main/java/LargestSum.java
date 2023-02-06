9454c31c1e42770414edb3fea81fc65a7a7f67fb
c0deblack <79178809+c0deblack@users.noreply.github.com>
2023-02-06 03:39:37 +0000

    Completed tests.

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -2,6 +2,25 @@
 import java.util.List;
 
 public class LargestSum {
+    public int getLargest(List<Integer> l, Integer largest)
+    {
+        Integer lhs = l.remove(0);
+
+        if(l.size() == 0) return largest;
+
+        if(largest == null)
+        {
+            largest = lhs + l.get(0);
+        }
+
+        for(Integer i : l)
+        {
+            Integer sum = lhs + i;
+            if(sum > largest) largest = sum;
+        }
+
+        return getLargest(l, largest);
+    }
     /**
      * Get the largest possible sum that can be obtained from a pair of values in the list. A number can't be added
      * to itself, unless there are duplicates.
@@ -10,6 +29,6 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        return getLargest(nums, null);
     }
 }
